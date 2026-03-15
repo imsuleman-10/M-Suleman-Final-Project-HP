5a188d75b1bde3ccedc2914886928851b95ae540
Suleman Zaheer <sulemanzaheer09@gmail.com>
2026-03-15 19:12:21 +0500

    Change API base URL to production endpoint

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api'
+  baseURL: 'https://m-suleman-final-project-hp.vercel.app/api'
 });
 
 api.interceptors.request.use(config => {
